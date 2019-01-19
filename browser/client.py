ee5bd5f0c2e9cc99eaf6f71e9544cddbed86044e
Bryce Corbitt <bscorbitt@wpi.edu>
2019-01-19 18:35:48 -0500

    some stuff

--- browser/client.py
+++ browser/client.py
@@ -1,10 +1,10 @@
 import socket
 def client_sock(curr_url):
     my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    my_sock.connect(('autoreg-119616.dyn.wpi.edu', 9995))
+    my_sock.connect(('blujay.dyn.wpi.edu', 9995))
     my_sock.sendall(b'POST / HTTP/1.1 HOST: ' + curr_url.encode())
     response = my_sock.recv(1024)
     my_sock.close()
     return(response)
 
-client_sock('https://www.youtube.com/watch?v=YC4pkJ2DZyc')
+print(client_sock('https://www.youtube.com/watch?v=hwMkMoSMK6o'))
