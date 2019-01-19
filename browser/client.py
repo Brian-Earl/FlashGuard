b67bfcb4b7f540289e655eac1c4e7dc1bad20089
Bryce Corbitt <bscorbitt@wpi.edu>
2019-01-19 18:48:39 -0500

    fixed client merge issue

--- browser/client.py
+++ browser/client.py
@@ -1,11 +1,7 @@
 import socket
 def client_sock(curr_url):
     my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-<<<<<<< HEAD
-    my_sock.connect(('', 9995))
-=======
-    my_sock.connect(('blujay.dyn.wpi.edu', 9995))
->>>>>>> ee5bd5f0c2e9cc99eaf6f71e9544cddbed86044e
+    my_sock.connect(('caroline.dyn.wpi.edu', 9995))
     my_sock.sendall(b'POST / HTTP/1.1 HOST: ' + curr_url.encode())
     response = my_sock.recv(1024)
     my_sock.close()
