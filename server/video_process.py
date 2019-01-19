f5ad7a6c34f3e50ede142c66b495e9c811f910b8
Bryce Corbitt <bscorbitt@wpi.edu>
2019-01-19 16:36:47 -0500

    Added methods to download and detect flashes in youtube videos

--- server/video_process.py
+++ server/video_process.py
@@ -3,9 +3,16 @@ import youtube_dl
 import numpy as np
 import os
 import time
+
+FLASH_MINIMUM = 3
+
 tmp_dir = 'temp/'
-ex = {'format': 'worstvideo', 'outtmpl': 'temp/temp.%(ext)s'}
+
+ex = {'format': 'worstvideo[vcodec^=avc1][fps=30]/worst[vcodec^=avc1][fps=30]/worstvideo[vcodec=vp9][fps=30]/worst[vcodec=vp9][fps=30]', 'outtmpl': 'temp/temp.%(ext)s', 'recode_video': 'webm'}
 ytdl = youtube_dl.YoutubeDL(ex)
+
+if not os.path.isdir(tmp_dir):
+    os.mkdir(tmp_dir)
 
 
 def clear_tmp():
@@ -16,7 +23,11 @@ def clear_tmp():
 
 def dl_tmp(url):
     clear_tmp()
-    ytdl.download([url])
+    try:
+        ytdl.download([url])
+        return 0
+    except youtube_dl.utils.DownloadError:
+        return 1
 
 
 def get_tmp():
@@ -26,34 +37,74 @@ def get_tmp():
 
 
 def process_video(path):
-    counter = 0
+    flash_distance = 0
     cap = cv2.VideoCapture(path)
     frames = []
+    flashes = []
     while cap.isOpened():
         # Capture frame-by-frame
         ret, frame = cap.read()
 
         if ret:
-            # Display the resulting frame
-            # cv2.imshow('frame', frame)
+            if flashes:
+                flash_distance += 1
+
             if not ret or cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            frames.append(frame)
+            frames.append(frame.astype(np.int16))
             if len(frames) > 3:
                 frames.pop(0)
-                difference = np.mean(np.abs(frames[2] - frames[0]))
-                if difference < 200:
-                    counter += 1
-                    cv2.imshow('frame', frames[0])
-                    cv2.imshow('frame', frames[2])
+
+                # get the net color change in pixel values
+                difference = np.abs(np.subtract(frames[2], frames[0]))
+                avg_color_change = np.mean(np.sum(difference, axis=2))
+                # average = np.mean(difference)
+
+                if avg_color_change > 200:
+                    flashes.append(flash_distance)
+                    flash_distance = 0
+                    frames.pop(0)
 
         else:
-            print(counter)
             cap.release()
+    return is_dangerous(flashes)
 
-#dl_tmp('https://www.youtube.com/watch?v=atkD-beZ9oI')
-#dl_tmp('https://www.youtube.com/watch?v=4N0qwuG3MaY')
-start = time.time()
-process_video(get_tmp())
-elapsed = time.time() - start
-print(elapsed)
+
+def process_video_url(link):
+    if dl_tmp(link) == 1:
+        print("Video could not be downloaded properly")
+        return
+
+    return process_video(get_tmp())
+
+
+def is_dangerous(flash_data):
+    num_flashes = len(flash_data)
+    if num_flashes == 0:
+        return False
+    avg_flash_distance = np.mean(flash_data)
+
+    # print(num_flashes)
+    # print(avg_flash_distance)
+
+    if (num_flashes >= FLASH_MINIMUM) and (num_flashes/avg_flash_distance) > 1:
+        return True
+    return False
+
+
+# https://www.youtube.com/watch?v=atkD-beZ9oI # baseline test
+# https://www.youtube.com/watch?v=Yw_YDvLWKnY # surreal video
+# https://www.youtube.com/watch?v=OCpzajWSp6I # mlg video
+# https://www.youtube.com/watch?v=FVY5uZ18-x8 #pokemon video
+
+
+def main():
+    start = time.time()
+    results = process_video_url('https://www.youtube.com/watch?v=Yw_YDvLWKnY')
+    elapsed = time.time() - start
+    print(results)
+    print(elapsed)
+
+
+if __name__ == '__main__':
+    main()
