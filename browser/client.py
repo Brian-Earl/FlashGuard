b57d6e10901b8c33ded997abf7656e02ca2fe0eb
Jeffrey Harnois <JeffHarnois@autoreg-119616.dyn.wpi.edu>
2019-01-19 15:40:46 -0500

    client socket as function

--- browser/client.py
+++ browser/client.py
@@ -1,9 +1,9 @@
-
 import socket
+def client_sock(curr_url):
+    my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    my_sock.connect(('autoreg-119616.dyn.wpi.edu', 9997))
+    my_sock.sendall(b'POST / HTTP/1.1 HOST: ' + curr_url)
+    response = my_sock.recv(1024)
+    my_sock.close()
 
-mySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySock.connect(('autoreg-119616.dyn.wpi.edu', 9997))
-mySock.sendall(b'POST / HTTP/1.1 HOST: www.youtube.com/watch?v=XTbTykMlhOk')
-response = mySock.recv(1024)
-print(response)
-mySock.close()
+client_sock(b'https://www.youtube.com/watch')
