8d05476b20a51d2b4e1ca38c728c0851a52192e9
Bryce Corbitt <bscorbitt@wpi.edu>
2019-01-19 22:29:22 -0500

    Video scan results are now stored persistently for efficiency

--- server/extension_handler.py
+++ server/extension_handler.py
@@ -3,7 +3,16 @@ import socket
 import time
 import video_process
 import json
+import pickle
+import os
 
+save_dir = 'processed_vids'
+if os.path.exists(save_dir):
+    with open(save_dir, 'rb') as f:
+        processed_vids = pickle.load(f)
+
+else:
+    processed_vids = {}
 
 
 def make_server():
@@ -25,8 +34,16 @@ def make_server():
         if(b'youtube.com/watch' in data):
             str_data = data.decode()
             starting_index = str_data.index('http')
-            print(data[starting_index:])
-            response_val = video_process.process_video_url(str_data[starting_index:])
+            str_data = str_data[starting_index:]
+            print(str_data)
+            if str_data in list(processed_vids.keys()):
+                response_val = processed_vids[str_data]
+            else:
+                response_val = video_process.process_video_url(str_data)
+                if response_val:
+                    processed_vids[str_data] = response_val
+                    with open(save_dir, 'wb') as w:
+                        pickle.dump(processed_vids, w, protocol=pickle.HIGHEST_PROTOCOL)
             c.sendall(str(response_val).encode())
         c.close()
 
